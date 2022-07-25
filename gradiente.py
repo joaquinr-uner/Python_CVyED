6472ab66b58935052121dee9fd8a17cd6db6054d
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2022-07-25 12:52:54 -0300

    Update gradiente.py

--- gradiente.py
+++ gradiente.py
@@ -1,3 +1,6 @@
 def gradiente(funcion_escalar, variables):
     matrix_funcion_escalar = sp.Matrix([funcion_escalar])#definimos una matriz de un solo elemento
     return matrix_funcion_escalar.jacobian(variables)
+def gradiente2(funcion_escalar, variables):
+    matrix_funcion_escalar = sp.Matrix([funcion_escalar])#definimos una matriz de un solo elemento
+    return matrix_funcion_escalar.jacobian(variables)
